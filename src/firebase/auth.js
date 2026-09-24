96c0b4876724b2c5200dfa37787d659ece78351b
shreyas <shreyasacharya256@gmail.com>
2026-09-24 15:43:20 +0530

    Set up Firebase authentication

---
+++ src/firebase/auth.js
@@ -0,0 +1,24 @@
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
+
+import app from "./config";
+
+const auth = getAuth(app);
+
+export const registerUser = (email, password) => {
+  return createUserWithEmailAndPassword(auth, email, password);
+};
+
+export const loginUser = (email, password) => {
+  return signInWithEmailAndPassword(auth, email, password);
+};
+
+export const logoutUser = () => {
+  return signOut(auth);
+};
+
+export { auth };
---
+++ src/firebase/config.js
@@ -0,0 +1,9 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  // Paste your Firebase configuration here
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
